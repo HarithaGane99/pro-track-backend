8f8f4a9f9efd38e2e64e6eb5fb699ae3e11a3732
HarithaGane99 <haritha.99ganegoda@gmail.com>
2026-02-16 17:00:22 +0530

    Implemented user registration with password hashing

--- app/main.py
+++ app/main.py
@@ -1,19 +1,30 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, status
 from sqlalchemy.orm import Session
-from sqlalchemy import text
-from .database import get_db
+from passlib.context import CryptContext
+from . import models, schemas, database
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "ProTrack API is running!"}
 
-@app.get("/db-test")
-def test_db_connection(db: Session = Depends(get_db)):
-    try:
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto", bcrypt__ident="2b")
 
-        db.execute(text("SELECT 1"))
-        return {"status": "success", "message": "Database connected successfully!"}
-    except Exception as e:
-        return {"status": "error", "message": str(e)}
+
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+
+@app.post("/register", response_model=schemas.UserOut, status_code=status.HTTP_201_CREATED)
+def register_user(user: schemas.UserCreate, db: Session = Depends(database.get_db)):
+   
+    db_user = db.query(models.User).filter(models.User.username == user.username).first()
+    if db_user:
+        raise HTTPException(status_code=400, detail="Username already registered")
+    
+    
+    hashed_pwd = hash_password(user.password)
+    new_user = models.User(username=user.username, password_hash=hashed_pwd, role=user.role)
+    
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
