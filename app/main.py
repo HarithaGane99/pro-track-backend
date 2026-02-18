7a73e93b5729a495a2614065acd2fe5ed7a03af9
HarithaGane99 <haritha.99ganegoda@gmail.com>
2026-02-18 11:13:44 +0530

    Implemented JWT Login and Token generation

--- app/main.py
+++ app/main.py
@@ -1,9 +1,20 @@
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from passlib.context import CryptContext
 from . import models, schemas, database
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from fastapi.security import OAuth2PasswordBearer
+
+load_dotenv()
+
 
 app = FastAPI()
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto", bcrypt__ident="2b")
@@ -12,6 +23,11 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto", bcrypt__ident=
 def hash_password(password: str):
     return pwd_context.hash(password)
 
+
+# Configuration
+SECRET_KEY = "your_very_secret_key_here" 
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 @app.post("/register", response_model=schemas.UserOut, status_code=status.HTTP_201_CREATED)
 def register_user(user: schemas.UserCreate, db: Session = Depends(database.get_db)):
@@ -28,3 +44,31 @@ def register_user(user: schemas.UserCreate, db: Session = Depends(database.get_d
     db.commit()
     db.refresh(new_user)
     return new_user
+
+
+@app.post("/login")
+def login(user_credentials: schemas.UserCreate, db: Session = Depends(database.get_db)):
+   
+    user = db.query(models.User).filter(models.User.username == user_credentials.username).first()
+    
+    if not user:
+        raise HTTPException(status_code=403, detail="Invalid Credentials")
+
+    
+    if not verify_password(user_credentials.password, user.password_hash):
+        raise HTTPException(status_code=403, detail="Invalid Credentials")
+
+  
+    access_token = create_access_token(data={"user_id": user.id, "role": user.role})
+
+    return {"access_token": access_token, "token_type": "bearer"}
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
